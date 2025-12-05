b559e38702363e164f19a6c02829c289c6ba3fa9
Newwiens <158727220+Newwiens@users.noreply.github.com>
2025-12-05 06:14:58 +0000

    test hero update

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <>
-      <div className="min-h-screen W bg-green-800 flex items-center justify-center">
-        <h1 className="text-3xl font-bold text-white">Hello World</h1>
+      <div className="min-h-screen W bg-white flex items-center justify-center">
+        <h1 className="text-3xl font-bold text-black">Hello World</h1>
       </div>
     </>
   );
