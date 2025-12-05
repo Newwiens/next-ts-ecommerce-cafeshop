e524807a54d60bea4b4ff6cd699820f57bd20ef0
Newwiens <158727220+Newwiens@users.noreply.github.com>
2025-12-05 06:17:49 +0000

    menu color update

--- frontend/src/app/menu/page.tsx
+++ frontend/src/app/menu/page.tsx
@@ -2,7 +2,7 @@
 import ProductList from "@/components/menu/ProductList";
 export default function MenuPage() {
   return (
-    <main className="min-h-screen bg-green-500 text-slate-100">
+    <main className="min-h-screen bg-white text-slate-100">
       <h1 className="text-3xl font-bold mb-4">Menu</h1>
       <p>Hier komt straks alle iced coffee & bubble tea 🍹</p>
 
